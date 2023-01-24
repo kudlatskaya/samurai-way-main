8e3fdd96f585619ee96f568596096d464e601c84
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-01-24 12:56:33 +0300

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Technologies />
+        </div>
+    );
 }
+
+const Header = () => {
+    return (
+        <div>
+            <a href="">Home</a>
+            <a href="">News Feed</a>
+            <a href="">Messages</a>
+        </div>
+    );
+}
+
+const Technologies = () => {
+    return (
+        <ul>
+            <li>css</li>
+            <li>html</li>
+            <li>js</li>
+            <li>react</li>
+        </ul>
+    );
+}
+
+
 
 export default App;
