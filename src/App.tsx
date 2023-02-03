64126678ba50e55d1e38f0e986b82d98fe273ffd
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-02-03 16:56:04 +0300

    add css moduls

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className='header'></header>
-            <nav className='nav'>
-                <div><a href="">Profile</a></div>
-                <div><a href="">Messages</a></div>
-                <div><a href="">News</a></div>
-                <div><a href="">Music</a></div>
-                <div><a href="">Settings</a></div>
-            </nav>
-            <main className='content'>
-                <div className='img'></div>
-                <div>
-                    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShCTtwxGDbjnpPgVZznNqUH757TIfzheMk6w&usqp=CAU" alt=""/></div>
-                    <div></div>
-                </div>
-                <div>My posts
-                    <div>new post</div>
-                </div>
-                <div>
-                    <div>post1</div>
-                    <div>post1</div>
-                    <div>post1</div>
-                </div>
-            </main>
-            <footer></footer>
+            <Header />
+            <Navbar />
+            <Profile />
         </div>
     );
 }
