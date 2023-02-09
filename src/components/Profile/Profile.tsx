a50bc452a78464b750c248c9d195a6f4075bca53
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-02-09 15:49:00 +0300

    add dialogs component

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <main className={s.content}>
+        <main>
             <div className={s.img}></div>
             <div className={s.userInfo}>
                 <div className={s.userAvatar}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShCTtwxGDbjnpPgVZznNqUH757TIfzheMk6w&usqp=CAU" alt=""/></div>
