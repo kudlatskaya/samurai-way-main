9506ef1cc377964678bd60501e83cc4c16b04a97
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-03-17 09:53:50 +0300

    state refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import state, {subscribe} from "./redux/state";
-import {addPost, StateType, updateNewPostText} from './redux/state'
-
+import store, {StateType}  from "./redux/state";
 
 export let renderEntireTree = (state: StateType) => {
     ReactDOM.render(
-        <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>,
+        <App state={store.getState()} addPost={store.addPost} updateNewPostText={store.updateNewPostText}/>,
         document.getElementById('root')
     );
 }
 
 
 
-renderEntireTree(state);
-subscribe(renderEntireTree);
+renderEntireTree(store.getState());
+store.subscribe(renderEntireTree);
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,4 +1,3 @@
-
 export type MessageType = {
     id: number,
     message: string,
@@ -27,56 +26,72 @@ export type StateType = {
     sidebar: [],
 }
 
-let state: StateType = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi', likesCount: 12},
-            {id: 2, message: 'By', likesCount: 1},
-            {id: 3, message: 'Hello', likesCount: 10},
-            {id: 4, message: 'Good by', likesCount: 11},
-        ],
-        newPostText: 'it-kamasutra.c',
+export type StoreType = {
+    _state: StateType,
+    getState: () => StateType,
+    addPost: () => void,
+    updateNewPostText: (newText: string) => void,
+    _rerenderEntireTree: (state: StateType) => void,
+    subscribe: (observer: (state: StateType) => void) => void,
+}
+
+let store: StoreType = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi', likesCount: 12},
+                {id: 2, message: 'By', likesCount: 1},
+                {id: 3, message: 'Hello', likesCount: 10},
+                {id: 4, message: 'Good by', likesCount: 11},
+            ],
+            newPostText: 'it-kamasutra.c',
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Andrey'},
+                {id: 3, name: 'Sasha'},
+                {id: 4, name: 'Viktor'},
+                {id: 5, name: 'Masha'},
+                {id: 6, name: 'Valera'},
+            ],
+            messages: [
+                {id: 1, message: 'hi'},
+                {id: 2, message: 'hi hi'},
+                {id: 3, message: 'hi hi hi'},
+                {id: 4, message: 'hi hi hi hi'},
+            ],
+        },
+        sidebar: [],
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Andrey'},
-            {id: 3, name: 'Sasha'},
-            {id: 4, name: 'Viktor'},
-            {id: 5, name: 'Masha'},
-            {id: 6, name: 'Valera'},
-        ],
-        messages: [
-            {id: 1, message: 'hi'},
-            {id: 2, message: 'hi hi'},
-            {id: 3, message: 'hi hi hi'},
-            {id: 4, message: 'hi hi hi hi'},
-        ],
+
+    getState() {
+        return this._state;
     },
-    sidebar: [],
+
+    addPost() {
+        const newPost: PostType = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0,
+        }
+
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._rerenderEntireTree(this._state);
+    },
+
+    updateNewPostText(newText: string) {
+        this._state.profilePage.newPostText = newText;
+        this._rerenderEntireTree(this._state);
+    },
+
+    _rerenderEntireTree() {},
+
+    subscribe(observer) {
+        this._rerenderEntireTree = observer;
+    },
 }
 
-let renderEntireTree = (state: StateType) => {}
 
-export const addPost = () => {
-    const newPost: PostType = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0,
-    }
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    renderEntireTree(state);
-}
-
-export const updateNewPostText = (newText: string) => {
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state);
-}
-
-export const subscribe = (observer: (state: StateType) => void) => {
-    renderEntireTree = observer;
-}
-
-export default state;
+export default store;
