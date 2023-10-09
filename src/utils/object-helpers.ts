ce427d9ffe983edb5611d10475f8d46c6ed1f1e8
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-10-09 09:11:40 +0300

    usersReducer fixed

--- src/utils/object-helpers.ts
+++ src/utils/object-helpers.ts
@@ -1,8 +1,8 @@
 import {UserType} from "../state/reducers/usersReducer";
 
-export const updateOnbjectInArray = (items: UserType[], itemId: number, objPropName: string, newObjProps: {}) => {
+export const updateOnbjectInArray = (items: UserType[], itemId: number, objPropName: string , newObjProps: {}) => {
     return items.map(item => {
-        if (item[objPropName] === itemId) {
+        if (item[objPropName as keyof UserType] === itemId) {
             return {...item, ...newObjProps}
         }
         return item
