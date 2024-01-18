410a797df60be1294fcd05cbf8cdf8a525b88f84
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2024-01-18 14:42:35 +0300

    profile adaptive completed

--- src/components/Users/UsersSearchForm.tsx
+++ src/components/Users/UsersSearchForm.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Field, Form, Formik} from "formik";
 import {FilterType} from "../../state/reducers/usersReducer";
-import {Input, InputAdornment} from "@mui/material";
+import {InputAdornment} from "@mui/material";
 import cs from "../common/common.module.css";
 import {accentColor, elementBgColor} from "../../constants";
 import {toggleFocus} from "../../utils/forms";
@@ -45,13 +45,19 @@ const UsersSearchForm: React.FC<PropsType> = React.memo(({onFilterChanged}) => {
                             <div id='user-input-block' className={cs.inputBlock}
                                  onFocus={(e) => toggleFocus(e, accentColor)}
                                  onBlur={(e) => toggleFocus(e, elementBgColor)}>
-                                {/*<Input className={cs.inputField} name="term"*/}
-                                {/*       type={"text"} placeholder={"Enter user name"}*/}
-                                {/*       id="user-input"*/}
-                                {/*/>*/}
 
-                                    <Field id="user-input" className={cs.inputField} type="text" name="term" placeholder={"Enter user name"}/>
+                                <Field id="user-input" className={cs.inputField} type="text" name="term"
+                                       placeholder={"Enter user name"}/>
 
+                            </div>
+                            <div className={`${cs.inputBlock} ${s.filter}`}
+                                 onFocus={(e) => toggleFocus(e, accentColor)}
+                                 onBlur={(e) => toggleFocus(e, elementBgColor)}>
+                                <Field as="select" name="friend">
+                                    <option value="null">All</option>
+                                    <option value="true">Only followed</option>
+                                    <option value="false">Only unfollowed</option>
+                                </Field>
                             </div>
                             <div className={s.find}>
                                 <button id={'user-find'} className={ls.loginButton} type={'submit'}
@@ -62,15 +68,7 @@ const UsersSearchForm: React.FC<PropsType> = React.memo(({onFilterChanged}) => {
                                 </button>
                             </div>
                         </div>
-                        <div className={`${cs.inputBlock} ${s.filter}`}
-                             onFocus={(e) => toggleFocus(e, accentColor)}
-                             onBlur={(e) => toggleFocus(e, elementBgColor)}>
-                            <Field as="select" name="friend">
-                                <option value="null">All</option>
-                                <option value="true">Only followed</option>
-                                <option value="false">Only unfollowed</option>
-                            </Field>
-                        </div>
+
                         {/*<ErrorMessage name="password" component="div"/>*/}
 
                     </div>
